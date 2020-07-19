ec403a880ed80d097f11b09cf7a4f8b5108fb12b
gabyballester <gaby.ballester@gmail.com>
2020-07-19 20:15:02 +0200

    Recuperamos el array de películas que nos llega desde la página tras nuestra petición con axios

--- src/containers/Movies/Movies.jsx
+++ src/containers/Movies/Movies.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Movies = props => {
-   return <div className="movies">
-        Tipo de Películas: {props.match.params.movieType}
-    </div>
-}
+const Movies = (props) => {
+  const movieType = props.match.params.movieType;
+  const api_key = "6944b42404ae9b9ce5b0c403ec0cd6a7";
+  const language = "es-ES";
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieType}?api_key=${api_key}&language=${language}`
+      )
+      .then((res) => setMovies(res.data.results))
+      .catch(console.error);
+  }, [movieType]);
+    return <div className="movies">
+        Tipo de Películas: {movieType}
+    </div>;
+};
 
 export default Movies;
